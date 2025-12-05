39f5c3be610989a896c00ba51ad1cc4c84ce5280
Jb <deslandes.jb@gmail.com>
2025-12-05 22:32:16 +0100

    added mongoose + changes on app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,9 +10,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
