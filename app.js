6e32bc83aac62c016d9fb70ee674cad03f04c032
Jb <deslandes.jb@gmail.com>
2025-12-09 12:55:04 +0100

    schema & mongoose connection

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('../models/connection');
 require('dotenv').config();
 
 var express = require('express');
--- schema/product.js
+++ schema/product.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 
+// sub-doc
+const pictureSchema = mongoose.Schema({
+	title: String,
+	url: String,
+});
+const avisSchema = mongoose.Schema({
+	content: String,
+	note: Number,
+});
+const sellersSchema = mongoose.Schema({
+	seller: String,
+	avis: avisSchema,
+	url: String,
+	price: Number,
+});
+
 const productSchema = mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	email: String,
-	inscriptionDate: Date,
+	name: String,
+	desc: String,
+	picture: pictureSchema,
+	brand: String,
+	categorie: String,
+	sellers: sellersSchema,
 });
 
 const Product = mongoose.model('products', productSchema);
