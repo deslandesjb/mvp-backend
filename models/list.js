b9f985e61a2cb59f044e73c6ca8b5d9923d1d93e
Azeddine <azederg@gmail.com>
2025-12-10 09:30:53 +0100

    modification: transfert des routes users/list sur lists.js

--- models/list.js
+++ models/list.js
@@ -4,6 +4,7 @@ const listsSchema = mongoose.Schema({
     name: String,
     idUser:{type: mongoose.Schema.Types.ObjectId, ref:'users'},
     idProduct:[{type: mongoose.Schema.Types.ObjectId, ref:'products'}],
+    done:Boolean,
 });
 
 const List = mongoose.model('lists', listsSchema);
