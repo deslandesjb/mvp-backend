195c2da2839d2c03e1e5526e744d7b8f56507cb9
Azeddine <azederg@gmail.com>
2025-12-10 17:27:38 +0100

    route delete mais pas fini

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.post('/signup', (req, res) => {
 	if (!checkBody(req.body, ["firstname", "lastname", "password", "mail"])) {
 	  res.json({ result: false, error: "Missing or empty fields" });
 	   return;
-	 }
+	}
 
 	const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 	if (!regex.test(req.body.mail)) {
