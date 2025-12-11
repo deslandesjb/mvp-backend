ee979854da843f4591112d945adb18589a217454
Azeddine <azederg@gmail.com>
2025-12-11 17:41:44 +0100

    changment du renvoie de la route get liste

--- routes/lists.js
+++ routes/lists.js
@@ -6,12 +6,54 @@ const Product = require('../models/product');
 
 /* GET Lists. */
 router.get('/:idUser', function (req, res) {
-  const idUser = req.params.idUser
-  List.find({ idUser: idUser })
-    .populate('lists')
-    .then(result => {
-      res.json({ result: true, listsUser: result })
+  const idUser = req.params.idUser;
+
+  List.find({ idUser })
+    .populate('idProduct')
+    .then(lists => {
+
+      const formattedLists = lists.map(list => {
+
+        return {
+          _id: list._id,
+          name: list.name,
+          idUser: list.idUser,
+          products: list.idProduct.map(product => {
+
+            // calcul prix moyen
+            const priceMoy = product.sellers && product.sellers.length
+              ? product.sellers.reduce((sum, s) => sum + s.price, 0) / product.sellers.length
+              : 0;
+
+            // calcul note moyenne
+            const allNotes = product.sellers
+              ? product.sellers.flatMap(s => s.avis.map(a => a.note))
+              : [];
+
+            const noteMoy = allNotes.length
+              ? allNotes.reduce((sum, n) => sum + n, 0) / allNotes.length
+              : 0;
+
+            return {
+              id: product._id,
+              name: product.name,
+              desc: product.desc,
+              picture: product.picture,
+              priceMoy: priceMoy.toFixed(2),
+              noteMoy: noteMoy.toFixed(2),
+            };
+          })
+        };
+
+      });
+
+      res.json({ result: true, listsUser: formattedLists });
+
     })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ result: false, error });
+    });
 });
 /* Post newList. */
 router.post('/newLists/:token/', function (req, res) {
@@ -119,22 +161,22 @@ router.post('/addToLists/:token/:idProduct/:idList', async (req, res) => {
 });
 /* Post removeList. */
 router.delete('/removeList/:idList', (req, res) => {
-  const {idList,idUser} = req.params
+  const { idList, idUser } = req.params
   List.deleteOne({ _id: idList }).then(() => {
-  User.findOneAndUpdate(
+    User.findOneAndUpdate(
       { lists: { $in: [idList] } },
       { $pull: { lists: idList } },
-    ).then(()=>{
+    ).then(() => {
       res.json({ result: true, list: "Supprimé !" });
     })
     // ------------ avec idUser pour être sur  en rajouter un params idUser ----------
-  // User.findByIdAndUpdate(
-  //     idUser,
-  //     { $pull: { lists: idList } },
-  //   ).then(()=>{
+    // User.findByIdAndUpdate(
+    //     idUser,
+    //     { $pull: { lists: idList } },
+    //   ).then(()=>{
 
-  //     res.json({ result: true, list: "Supprimé !" });
-  //   })
+    //     res.json({ result: true, list: "Supprimé !" });
+    //   })
   })
 })
 
