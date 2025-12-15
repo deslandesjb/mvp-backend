9329b3e66a9231079414c3ee810536aec072d8af
Jb <deslandes.jb@gmail.com>
2025-12-15 12:17:18 +0100

    back to initial code

--- routes/lists.js
+++ routes/lists.js
@@ -7,9 +7,11 @@ const Product = require('../models/product');
 /* GET Lists. */
 router.get('/:token', function (req, res) {
 	const token = req.params.token;
-	User.findOne({ token: { $in: [token] } }).then(user => {
-		const idUser = user._id
-		List.find({ idUser })
+	User.findOne({token: {$in: [token]}}).then((user) => {
+		const idUser = user._id;
+		console.log(idUser);
+
+		List.find({idUser})
 			.populate('idProduct')
 			.then((lists) => {
 				const formattedLists = lists.map((list) => {
@@ -41,26 +43,27 @@ router.get('/:token', function (req, res) {
 					};
 				});
 
-				res.json({ result: true, listsUser: formattedLists });
+				res.json({result: true, listsUser: formattedLists});
 			})
 			.catch((error) => {
 				console.error(error);
-				res.status(500).json({ result: false, error });
+				res.status(500).json({result: false, error});
 			});
-	})
+	});
 });
+
 /* Post newList. */
 router.post('/newLists/:token/', function (req, res) {
 	const token = req.params.token;
 	const name = req.body.name;
 
 	// $in verifie dans un tableau dans le cas de user il contient un tableau de token
-	User.findOne({ token: { $in: [token] } }).then((user) => {
+	User.findOne({token: {$in: [token]}}).then((user) => {
 		if (!user) {
-			return res.json({ result: false, response: 'User not connected !' });
+			return res.json({result: false, response: 'User not connected !'});
 		}
 
-		List.find({ idUser: user._id, name: name }).then((found) => {
+		List.find({idUser: user._id, name: name}).then((found) => {
 			console.log('user', found);
 			if (found.length < 1) {
 				const newList = new List({
@@ -70,12 +73,12 @@ router.post('/newLists/:token/', function (req, res) {
 					done: false,
 				});
 				newList.save().then((list) => {
-					User.findByIdAndUpdate(user._id, { $push: { lists: list._id } }).then(() => {
-						return res.json({ result: true, newList: list });
+					User.findByIdAndUpdate(user._id, {$push: {lists: list._id}}).then(() => {
+						return res.json({result: true, newList: list});
 					});
 				});
 			} else {
-				return res.json({ result: false, response: 'Name already used !' });
+				return res.json({result: false, response: 'Name already used !'});
 			}
 		});
 	});
@@ -83,13 +86,13 @@ router.post('/newLists/:token/', function (req, res) {
 /* Post listDone. */
 router.post('/listDone/:idList', async (req, res) => {
 	try {
-		const list = await List.findOne({ _id: req.params.idList });
+		const list = await List.findOne({_id: req.params.idList});
 		console.log(list);
 		if (!list) {
-			return res.json({ result: false, response: 'List not found !' });
+			return res.json({result: false, response: 'List not found !'});
 		}
 		const nouveauStatut = !list.done;
-		const updateResult = await List.updateOne({ _id: list._id }, { done: nouveauStatut });
+		const updateResult = await List.updateOne({_id: list._id}, {done: nouveauStatut});
 		return res.json({
 			result: true,
 			done: nouveauStatut,
@@ -97,32 +100,32 @@ router.post('/listDone/:idList', async (req, res) => {
 		});
 	} catch (err) {
 		console.error(err);
-		return res.status(500).json({ result: false, response: 'Server error' });
+		return res.status(500).json({result: false, response: 'Server error'});
 	}
 });
 /* Post addToLists. */
 router.post('/addToLists/:token/:idProduct/:idList', async (req, res) => {
-	const { token, idProduct, idList } = req.params;
+	const {token, idProduct, idList} = req.params;
 
 	try {
-		const user = await User.findOne({ token });
+		const user = await User.findOne({token});
 		if (!user) {
-			return res.json({ result: false, response: 'User not connected !' });
+			return res.json({result: false, response: 'User not connected !'});
 		}
 
 		const product = await Product.findById(idProduct);
 		if (!product) {
-			return res.json({ result: false, response: 'Product not found !' });
+			return res.json({result: false, response: 'Product not found !'});
 		}
 
 		// Vérifie si la liste appartient à l'utilisateur
 		if (!user.lists.includes(idList)) {
-			return res.json({ result: false, response: 'List not found or not belonging to user!' });
+			return res.json({result: false, response: 'List not found or not belonging to user!'});
 		}
 
 		const list = await List.findById(idList);
 		if (!list) {
-			return res.json({ result: false, response: 'List not found !' });
+			return res.json({result: false, response: 'List not found !'});
 		}
 
 		// Vérifie si le produit est déjà dans la liste
@@ -130,27 +133,24 @@ router.post('/addToLists/:token/:idProduct/:idList', async (req, res) => {
 
 		let update;
 		if (!productInList) {
-			update = await List.findByIdAndUpdate(idList, { $push: { idProduct: product._id } });
+			update = await List.findByIdAndUpdate(idList, {$push: {idProduct: product._id}});
 		} else {
-			update = await List.findByIdAndUpdate(idList, { $pull: { idProduct: product._id } });
+			update = await List.findByIdAndUpdate(idList, {$pull: {idProduct: product._id}});
 		}
 
-		return res.json({ result: true, updated: update });
+		return res.json({result: true, updated: update});
 	} catch (error) {
 		console.error(error);
-		res.json({ result: false, error: 'Internal server error' });
+		res.json({result: false, error: 'Internal server error'});
 	}
 });
 /* Post removeList. */
 router.delete('/removeList/:idList', (req, res) => {
-	const { idList, idUser } = req.params
-	List.deleteOne({ _id: idList }).then(() => {
-		User.findOneAndUpdate(
-			{ lists: { $in: [idList] } },
-			{ $pull: { lists: idList } },
-		).then(() => {
-			res.json({ result: true, list: "Supprimé !" });
-		})
+	const {idList, idUser} = req.params;
+	List.deleteOne({_id: idList}).then(() => {
+		User.findOneAndUpdate({lists: {$in: [idList]}}, {$pull: {lists: idList}}).then(() => {
+			res.json({result: true, list: 'Supprimé !'});
+		});
 		// ------------ avec idUser pour être sur  en rajouter un params idUser ----------
 		// User.findByIdAndUpdate(
 		//     idUser,
@@ -159,12 +159,11 @@ router.delete('/removeList/:idList', (req, res) => {
 
 		//     res.json({ result: true, list: "Supprimé !" });
 		//   })
-	})
-})
+	});
+});
 
 //     res.json({ result: true, list: "Supprimé !" });
 //   })
-
 
 //     res.json({ result: true, list: "Supprimé !" });
 //   })
